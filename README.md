ac5ad5b2aaf99faee3ecec2d73d0b006158761a8
lemonorangeapple <jcj1947725596@hotmail.com>
2023-06-24 05:25:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,14 @@
+## 部署
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/jcj-software/comment)
+
+点一下就得，
+
+然后设置环境变量：
+|        名称        |                   值                  |
+|:------------------:|:-------------------------------------:|
+|     REDIS_HOST     |            Redis数据库地址            |
+|     REDIS_PORT     |            Redis数据库端口            |
+|   REDIS_PASSWORD   |            Redis数据库密码            |
+
+点击部署就行
+
---
+++ index.py
@@ -0,0 +1,67 @@
+import redis
+from flask import *
+from datetime import timedelta
+import os
+from pysmx.SM3 import SM3
+
+# Hash function
+def sm3_hash(data: str):
+    s = SM3()
+    s.update(data)
+    return str(s.hexdigest())
+
+# Create server
+app = Flask(__name__)
+
+# Load envrion
+redis_host = str(os.getenv("REDIS_HOST"))
+redis_port = str(os.getenv("REDIS_PORT"))
+redis_password = str(os.getenv("REDIS_PASSWORD"))
+
+# Connect redis database
+db = redis.Redis(host = redis_host,
+                port = redis_port,
+                password = redis_password,
+                decode_responses = True)
+
+# Load message data from redis database
+messages = []
+message = db.get("message")
+if message is not None:
+    message = message.split(";")
+    message.remove('')
+    for i in message:
+        m = i.split(':')
+        messages.append({'username': str(m[0]), 'message': str(m[1])})
+else:
+    message = ""
+
+
+# The master page
+@app.route("/")
+def index():
+    return redirect("/comment")
+        
+# The comment page
+@app.route("/comment", methods = ["GET", "POST"])
+def chat():
+    if request.method == "GET":
+        # If user is online, render the html page file.
+        return render_template("comment.html", messages = messages)
+    elif request.method == "POST":
+        # Get "message" data from request for send message
+        username = request.values.get("username")
+        mess = request.values.get("message")
+        if mess is None:
+            return redirect("/")
+        if len(mess) > 1000:
+            return "The message is too long."
+        if ':' in mess or ';' in mess:
+            return "The message contains special characters."
+        global message
+        message = str(message) + str(username) + ":" + str(mess) + ";"
+        messages.append({"username": str(username), "message": str(mess)})
+        global cnt
+        cnt += 1
+        db.set("message", message)
+        return redirect("/chat")
