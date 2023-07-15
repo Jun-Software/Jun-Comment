b0708db4976d61d20b216ab9c6ef11deea308772
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-15 17:07:00 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## 部署
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/lemonorangeapple/comment)
 
-点一下就行，
+点一下部署，
 
 然后设置环境变量：
 |        名称        |                   值                  |
@@ -9,6 +9,4 @@
 |     REDIS_HOST     |            Redis数据库地址            |
 |     REDIS_PORT     |            Redis数据库端口            |
 |   REDIS_PASSWORD   |            Redis数据库密码            |
-
-点击部署就行
-
+|     SESSION_KEY    |            Session加密密匙            |
--- index.py
+++ index.py
@@ -1,56 +1,94 @@
 import redis
 from flask import *
-from datetime import timedelta
 import os
 from pysmx.SM3 import SM3
 
-# Hash function
 def sm3_hash(data: str):
     s = SM3()
     s.update(data)
     return str(s.hexdigest())
 
-# Create server
 app = Flask(__name__)
 
-# Load envrion
 redis_host = str(os.getenv("REDIS_HOST"))
 redis_port = str(os.getenv("REDIS_PORT"))
 redis_password = str(os.getenv("REDIS_PASSWORD"))
+app.config['SECRET_KEY'] = str(os.getenv("SESSION_KEY"))
 
-# Connect redis database
 db = redis.Redis(host = redis_host,
                 port = redis_port,
                 password = redis_password,
                 decode_responses = True)
 
-# Load message data from redis database
 messages = []
 message = db.get("message")
 if message is not None:
-    message = message.split(";")
-    message.remove('')
-    for i in message:
+    temp = message.split(";")
+    temp.remove('')
+    for i in temp:
         m = i.split(':')
         messages.append({'username': str(m[0]), 'message': str(m[1])})
 else:
     message = ""
 
+users = []
+user = db.get("user")
+if user is not None:
+    temp = user.split(";")
+    temp.remove('')
+    for i in temp:
+        u = i.split(':')
+        users.append({'username': str(u[0]), 'password': str(u[1])})
+else:
+    user = ""
 
-# The master page
 @app.route("/")
 def index():
-    return redirect("/comment")
-        
-# The comment page
-@app.route("/comment", methods = ["GET", "POST"])
-def chat():
+    if session.get('login') is None or session.get('login') == False:
+        return redirect('/login')
+    return redirect("/chat")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if session.get('login') == True:
+        return redirect('/chat')
     if request.method == "GET":
-        # If user is online, render the html page file.
-        return render_template("comment.html", messages = messages)
+        return render_template("login.html")
     elif request.method == "POST":
-        # Get "message" data from request for send message
+        Type = request.values.get("type")
         username = request.values.get("username")
+        password = request.values.get("password")
+        global user
+        global users
+        if Type == 'register':
+            if username is None or password is None:
+                return redirect("/")
+            if len(username) > 20:
+                return "The username is too long."
+            if ':' in username or ';' in username:
+                return "The username contains special characters."
+            user = str(user) + str(username) + ":" + str(sm3_hash(password)) + ";"
+            users.append({"username": str(username), "password": str(sm3_hash(password))})
+            db.set("user", user)
+            session['login'] = True
+            session['username'] = username
+            return redirect("/chat")
+        elif Type == 'login':
+            for u in users:
+                if u['password'] == sm3_hash(password) and u['username'] == username:
+                    session['login'] = True
+                    session['username'] = username
+                    return redirect('/chat')
+            return "The username or password do not match."
+
+@app.route("/chat", methods = ["GET", "POST"])
+def chat():
+    if session.get('login') is None or session.get('login') == False:
+        return redirect('/login')
+    if request.method == "GET":
+        return render_template("chat.html", username = session.get('username'))
+    elif request.method == "POST":
+        username = session.get("username")
         mess = request.values.get("message")
         if mess is None:
             return redirect("/")
@@ -59,7 +97,12 @@ def chat():
         if ':' in mess or ';' in mess:
             return "The message contains special characters."
         global message
+        global messages
         message = str(message) + str(username) + ":" + str(mess) + ";"
         messages.append({"username": str(username), "message": str(mess)})
         db.set("message", message)
-        return redirect("/comment")
+        return redirect("/chat")
+
+@app.route('/api')
+def api():
+    return render_template("api.html", messages = messages)
